e221160dceea323ffaafbc9bce0e5296dd79217b
zhengmengtian <zhengmengtian@mngo.com>
2020-08-18 11:25:33 +0800

    1

--- store/index.js
+++ store/index.js
@@ -5,20 +5,8 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	state: {
-		fontSize: '',
-		lineHeight: '',
-		background: ''
 	},
 	mutations: {
-		setFontSize(state, res) {
-			state.fontSize = res;
-		},
-		setLineHeight(state, res) {
-			state.lineHeight = res;
-		},
-		setBackground(state, res) {
-			state.background = res;
-		}
 	},
 	actions: {
 		
